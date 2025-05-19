499ee13494bd966bb4c16cfdce9bdd6071c95fef
rivopelu <rivopelu12@gmail.com>
2025-05-19 21:19:36 +0700

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import Button from "./components/atoms/Button.tsx";
 
 export default function App() {
-    return (
-        <div className={"min-h-screen"}>
-            <div>
-                <div>VARIANT</div>
-                <Button variant={"outlined"}>Button</Button>
-                <Button variant={"solid"}>Button</Button>
-                <Button variant={"text"}>Button</Button>
+  return (
+    <div className={"min-h-screen"}>
+      <div>
+        <div>VARIANT</div>
+        <Button variant={"outlined"}>Button</Button>
+        <Button variant={"solid"}>Button</Button>
+        <Button variant={"text"}>Button</Button>
 
-                <div>COLOR</div>
-                <Button variant={"outlined"} color={"primary"}>Button</Button>
-                <Button variant={"solid"} color={"gray"}>Button</Button>
-                <Button variant={"text"} color={"error"}>Button</Button>
-                <Button variant={"text"} color={"info"}>Button</Button>
-                <Button variant={"text"} color={"white"}>Button</Button>
-
-            </div>
-        </div>
-    );
+        <div>COLOR</div>
+        <Button variant={"outlined"} color={"primary"}>
+          Button
+        </Button>
+        <Button variant={"solid"} color={"gray"}>
+          Button
+        </Button>
+        <Button variant={"text"} color={"error"}>
+          Button
+        </Button>
+        <Button variant={"text"} color={"info"}>
+          Button
+        </Button>
+        <Button variant={"text"} color={"white"}>
+          Button
+        </Button>
+      </div>
+    </div>
+  );
 }
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),     tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    // library entry and output settings
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "cottage-ui",
+      fileName: "cottage-ui",
+    },
+    // bundler options
+    // externalize react-related imports
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+  },
+});
